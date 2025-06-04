d068dfdfe4584c1591b95e2c6919c683b07ffa1d
Stijn van Beek <stijn.v.beek@gmail.com>
2025-06-04 12:57:45 +0200

    Create clone_4dsound.sh

---
+++ clone_4dsound.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/4dsound/min-devkit-projects source/projects
+git clone https://github.com/4dsound/vban source/projects/thirdparty/vban
+git submodule update --init --recursive
